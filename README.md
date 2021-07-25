0753aa13dae1a6cd594182e3aaf379bf9d90f4f7
cavalcant3 <jmatheusoc269@gmail.com>
2021-07-25 16:49:59 -0300

    bug fix

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# tela_login
-Treinando React com Ts
+# Tela de login com Material UI
+
+Apenas testando o framework de estilização
+
+## Authors
+
+- [@cavalcant3](https://www.github.com/cavalcant3)
+
+## Pacotes usados:
+
+Material UI core e Material ui Icons.
+
+## Lições aprendidas
+
+Material ui agiliza estilizações aplicações com react, uma ferramenta bem poderosa, porém bem pesada. Irei avaliar usar em projetos maiores.
+
+![Logo](https://cdn-media-1.freecodecamp.org/images/1*FDNeKIUeUnf0XdqHmi7nsw.png)
+
+## Tech Stack
+
+**Server:** No server
+**Client side:** ReactJS
+
+## Support
+
+Para dúvidas: jmatheusoc269@gmail.com
+
+## Screenshots
+
+![tela1.jpeg](/assets/img1.jpeg)
+![tela2.jpeg](/assets/img1.jpeg)
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/cavalcant3/tela_login.git
+```
+
+E execute localmente.
+O projeto não foi publicado
--- tela_login/src/Login.jsx
+++ tela_login/src/Login.jsx
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -12,13 +10,13 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { CheckBox } from '@material-ui/icons';
+
 
 
 function Dados(){
   return(
     <Typography variant='body2' color='textSecondary' align='center'>
-      {'build with <3 from Matheus Cavalcante'} <br />
+      {'made with <3 from Matheus Cavalcante'} <br />
       <Link color='inherit' href='github.com/cavalcant3' target="_blank">
         Visite meu github.
       </Link>
